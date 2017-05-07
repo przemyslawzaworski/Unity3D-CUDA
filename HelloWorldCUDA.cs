44802a6a6da687fed97ea304a52b1672af2de7d8
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-05-07 16:13:00 +0200

    Load HelloWorldCUDA.dll into Unity3D Engine

---
+++ HelloWorldCUDA.cs
@@ -0,0 +1,23 @@
+﻿//copy HelloWorldCUDA.dll into Assets/Plugins directory
+using System;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class HelloWorldCUDA : MonoBehaviour 
+{
+	[DllImport("HelloWorldCUDA.dll", EntryPoint = "CUDA_device_name")]
+	static extern IntPtr CUDA_device_name ();
+	IntPtr handle;
+	String caption;
+
+	void Start()
+	{
+		handle = CUDA_device_name ();
+		caption = Marshal.PtrToStringAnsi (handle);
+	}
+
+	void OnGUI() 
+	{
+		GUILayout.Label (caption);
+	}
+}
